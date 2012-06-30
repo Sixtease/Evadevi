4a8421c7197786469858155fd739e2dd1ac05076
Oldrich Kruza <sixtease+github@gmail.com>
2012-06-30 13:00:20 +0200

    Documented evadevi.pl

--- bin/evadevi.pl
+++ bin/evadevi.pl
@@ -75,3 +75,165 @@ Evadevi -- a chain of scripts to train HTK acoustic models from transcribed spee
     --test-wordlist data/wordlist/test-phones \
     --lm data/language-model/bigram.lat \
     --homedir ~/Evadevi/ [--outdir hmms/] [--workdir temp/]
+
+=head1 DESCRIPTION
+
+Evadevi trains a HMM from transcribed data. It currently operates only with
+MFCC-encoded audio files and only trains monophone models.
+
+The model is trained in five steps:
+
+=over 4
+
+=item 1. Initialization
+
+The model is initialized from global data variance
+
+=item 2. Short pause
+
+The short pause (C<sp>) model is added based on the model for silence.
+
+=item 3. Alignment
+
+The training data are force-aligned to the current model with a pruning treshold
+and the sentences that fail to be force-aligned are removed from the training
+dataset.
+
+=item 4. Triphones
+
+The models are split to contextual. This is currently unimplemented and skipped.
+
+=item 5. Mixtures
+
+The models get an extra mixture and performance is evaluated on heldout data.
+This is repeated until the performance stops growing.
+
+=back
+
+=head2 Options
+
+Options can be passed directly to evadevi.pl or as environment variables. Some
+options can only be set as env vars.
+
+=over 4
+
+=item --train-mfcc
+
+=item EV_train_mfcc
+
+Path to directory with training MFCC files (preferably one sentence per file).
+The files B<must> end with C<.mfcc> (not C<.mfc>).
+
+=item --train-transcription
+
+=item EV_train_transcription
+
+Path to transcription of the training audio files in MLF format (see HTK Book
+for its description).
+
+=item --train-wordlist
+
+=item EV_wordlist_train_phonet
+
+Path to phonetic dictionary covering the training data.
+
+=item --test-wordlist
+
+=item EV_wordlist_test_phonet
+
+Path to phonetic dictionary for recognition. This and the language model are
+necessary for finding the optimal count of mixtures as well as for evaluating
+the trained models.
+
+=item --lm
+
+=item EV_LM
+
+Path to language model in HTK lattice format.
+
+=item --m
+
+=item EV_use_triphones
+
+Specifying the C<-m> option or setting the C<EV_use_triphones> environment
+variable to an B<empty string> switches off training a triphone model. Since
+triphones are not yet implemented, this option is necessary.
+
+=item --homedie
+
+=item EV_homedir
+
+The directory where Evadevi resides. Precisely, the directory where the
+Makefile, config.sh and resources/ reside.
+
+The environment variable B<must> have a trailing slash. When specifying the
+option directly, the slash is automatically appended.
+
+Evadevi needs to know its homedir to operate; however, in most cases, it can
+figure it out itself. For this to work, it is important to run the binary
+directly and not via a symlink. (Adding evadevi.pl to PATH is OK.) If you want
+to run evadevi.pl from a link, then you have to specify the homedir.
+
+=item --outdir
+
+=item EV_outdir
+
+The directory where the final models are to be stored.
+
+=item --workdir
+
+=item EV_workdir
+
+The directory where the intermediate files are stored. Removing this directory
+after running has the effect of a C<make clean>.
+
+=item EV_min_mixures
+
+If this variable is set, the final HMM is guaranteed to have at least this many
+mixtures per phone. The rationale for this option is that sometimes, a local
+maximum at a lower mixture count would prevent a higher maximum to be found.
+
+Try a high value (~20) and see how the model precision changes with each split.
+
+=item EV_HVite_s
+
+The C<s> option to HVite (LM weight).
+
+=item EV_HVite_p
+
+The C<p> option to HVite (word insertion penalty).
+
+=item EV_HVite_t
+
+The C<t> option to HVite (pruning threshold).
+
+=item EV_iterN
+
+How many reestimations are performed after each step. C<EV_iter> controls all
+iterations, C<EV_iter1> controls the iterations after step 1 etc. All default
+to 2, except for C<EV_iter5> which defaults to 9 (9 reestimations after each
+split).
+
+=item --heldout-ratio
+
+=item EV_heldout_ratio
+
+Evadevi lays a part of the training data aside to test its models. These data
+are called I<heldout>. This option specifies, one of how many training sentences
+are held out. The default is 20, which means 5% of the training data are held
+out.
+
+=item EV_phones_count_file
+
+The set of monophones is extracted from the training dictionary. If this option
+is set, the count of the phones are written down to the file, whose path the
+option designates.
+
+=item EV_test_mfcc
+
+=item EV_test_transcription
+
+Testing MFCC directory and transcription file (like for training). Needed when
+you want to evaluate the HMM via C<make test>.
+
+=back
