aaaddaafba2200df40c1c352e4239dae73ae35ee
Jan Oldrich Kruza <sixtease+github@gmail.com>
2012-10-20 21:56:34 +0200

    step-var now iterates

--- bin/step-recalculate-variance.pl
+++ bin/step-recalculate-variance.pl
@@ -16,31 +16,54 @@ use HTKUtil::InitHmm qw(calculate_variance init_macros);
 
 my %opt = (
     f => $ENV{EV_HCompV_f},
+    iter => $ENV{EV_iter_var} || $ENV{EV_iter} || 2,
 );
 GetOptions(\%opt, qw(
     conf=s
     f=f
     indir=s
-    workdir=s
     outdir=s
     mfccdir=s
     mlf=s
     proto=s
+    iter=i
 ));
 
-my $scp_fn = "$opt{workdir}/mfcc.scp";
+my $scp_fn = "$opt{outdir}/aux/mfcc.scp";
 mlf2scp($opt{mlf}, $scp_fn, "$opt{mfccdir}/*.mfcc");
 
+print STDERR (' ' x 8), "calculating variance...\n";
 calculate_variance(
     '-C' => $opt{conf},
     '-f' => $opt{f},
     '-S' => $scp_fn,
-    '-M' => $opt{workdir},
+    '-M' => "$opt{outdir}/var",
     '-I' => $opt{mlf},
     ''   => $opt{proto},
 );
 
-init_macros($opt{proto}, "$opt{workdir}/vFloors", "$opt{outdir}/macros");
-cp("$opt{indir}/hmmdefs", "$opt{outdir}/hmmdefs");
+init_macros($opt{proto}, "$opt{outdir}/var/vFloors", "$opt{outdir}/base/macros");
+cp("$opt{indir}/hmmdefs", "$opt{outdir}/base/hmmdefs");
+
+print STDERR (' ' x 8), "training...\n";
+hmmiter(
+    ( map {;$_ => $opt{$_}} qw(iter outdir mfccdir conf mlf) ),
+    indir => "$opt{outdir}/base",
+    workdir => "$opt{outdir}/iterations",
+    phones => "$opt{indir}/phones",
+);
+
+print STDERR (' ' x 8 ), "evaluating...\n";
+{
+    my $score = HTKUtil::evaluate_hmm(
+        ( map {; $_ => $opt{$_} } qw(conf mfccdir) ),
+        hmmdir        => $opt{outdir},
+        workdir       => $ENV{EV_eval_workdir},
+        transcription => $ENV{EV_heldout_mlf},
+        LM            => $ENV{EV_LM},
+        wordlist      => $ENV{EV_default_wordlist},
+    );
+    print "$score->{raw}\n$score\n";
+}
 
 __END__
