7ff2e6f520810927af4eeb28beaf1dd475b3642f
Jan Oldrich Kruza <sixtease+github@gmail.com>
2014-03-15 14:39:41 +0100

    refactored recognition and evaluation

--- lib/JulLib.pm
+++ lib/JulLib.pm
@@ -10,6 +10,101 @@ our @EXPORT_OK = qw(evaluate_hmm);
 
 sub evaluate_hmm {
     my %opt = @_;
+    my $hmmdir = $opt{hmmdir} || die 'Missing directory with HMMs to test (hmmdir)';
+    my $trans_fn = $opt{transcription} || die 'Missing transcription file (transcription)';
+    my $workdir = $opt{workdir} || $hmmdir;
+    my $phones_fn = $opt{phones} || "$hmmdir/phones";
+    
+    my $recout_fn = recognize(%opt);
+    
+    my $mlf_out_fn = "$workdir/recout.mlf";
+    recout_to_mlf(
+        recout_fn => $recout_fn,
+        mlf_out_fn => $mlf_out_fn,
+    );
+    
+    my $score = evaluate_recout(%opt,
+        mlf_out_fn => $mlf_out_fn,
+        workdir => $workdir,
+        phones => $phones_fn,
+    );
+    
+    # save score next to hmmdefs
+    my $opened = open my $score_fh, '>', "$hmmdir/score";
+    if ($opened) {
+        print {$score_fh} $score->{raw};
+        close $opened;
+    }
+    else {
+        warn "Couldn't save score to '$hmmdir/score'";
+    }
+    
+    return $score;
+}
+
+sub evaluate_recout {
+    my %opt = @_;
+    my $trans_fn = $opt{transcription} || die 'Missing transcription file (transcription)';
+    my $workdir = $opt{workdir} || die 'Missing workdir';
+    my $phones_fn = $opt{phones} || die 'Missing phones';
+    my $mlf_out_fn = $opt{mlf_out_fn} || die 'Missing mlf_out_fn (MLF recout to evaluate)';
+    
+    my $results_fn = h(stringify_options(
+        ''   => 'HResults',
+        '-A' => '', '-D' => '', '-T' => 1,
+        '-z' => '</s>',
+        '-I' => $trans_fn,
+        ''   => [$phones_fn, $mlf_out_fn],
+    ), LANG => 'C', return_output  => 1);
+    my $line = '';
+    my $raw = '';
+    {
+        local @ARGV = $results_fn;
+        while (<ARGV>) {
+            $raw .= $_;
+            if (/Overall Results/ .. /WORD:/) {
+                $line = $_;
+            }
+        }
+    }
+    $line =~ /%Corr=(\S+?),/ or die "Unexpected results:\n$raw";
+    
+    return Score->new($1, $raw);
+}
+
+sub recout_to_mlf {
+    my %opt = @_;
+    
+    my $recout_fn = $opt{recout_fn};
+    my $mlf_out_fn = $opt{mlf_out_fn};
+    
+    open my $recout_fh, '<', $recout_fn or die "Couldn't open julius output file '$recout_fn': $!";
+    open my $mlf_out_fh, '>', $mlf_out_fn or die "Couldn't open '$mlf_out_fn' for writing: $!";
+    
+    print {$mlf_out_fh} "#!MLF!#\n";
+    my $in_walign = 0;
+    while (<$recout_fh>) {
+        if (my ($label) = /input MFCC file: (.+)/) {
+            if ($ENV{EV_label_transform}) {
+                for ($label) {
+                    eval $ENV{EV_label_transform};
+                }
+            }
+            print {$mlf_out_fh} qq("$label"\n);
+        }
+        m/-- word alignment --/ and $in_walign = 1;
+        m/=== end forced alignment ===/ and $in_walign and (print {$mlf_out_fh} ".\n"), $in_walign = 0;
+        if ($in_walign and my @m = /^\[\s*(\d+)\s+(\d+)\s*\]\s*([-\d.]+)\s+(\S+)/) {
+            next if $m[3] =~ /^</;
+            print {$mlf_out_fh} $m[0].'00000 '.$m[1]."00000 $m[3] $m[2]\n";
+        }
+    }
+    close $recout_fh;
+    close $mlf_out_fh;
+}
+
+sub recognize {
+    my %opt = @_;
     my $LMf = $opt{LMf} || die 'Missing language model (LMf)';
     my $LMb = $opt{LMb} || die 'Need backward language model (LMb)';
     my $hmmdir = $opt{hmmdir} || die 'Missing directory with HMMs to test (hmmdir)';
@@ -18,9 +113,8 @@ sub evaluate_hmm {
     my $mfccdir = $opt{mfccdir} || die 'Missing directory with testing MFCC files (mfccdir)';
     my $workdir = $opt{workdir} || $hmmdir;
     my $phones_fn = $opt{phones} || "$hmmdir/phones";
-    my $align = $opt{align} || '-walign';
+    my $align = $opt{align} || '-walign -palign';
     my $unk = $opt{unk} || '!!UNK';
-    my $segment_input = $opt{segment_input};    # probably to delete
     
     my $hmm_fn;
     if (-e "$hmmdir/hmmmodel") {
@@ -58,55 +152,7 @@ sub evaluate_hmm {
         -fallback1pass => '',
     });
     
-    my $mlf_out_fn = "$workdir/recout.mlf";
-    open my $recout_fh, '<', $recout_fn or die "Couldn't open julius output file '$recout_fn': $!";
-    open my $mlf_out_fh, '>', $mlf_out_fn or die "Couldn't open '$mlf_out_fn' for writing: $!";
-    
-    print {$mlf_out_fh} "#!MLF!#\n";
-    my $in_walign = 0;
-    while (<$recout_fh>) {
-        /input MFCC file: (.*)/ and print {$mlf_out_fh} qq("$1"\n);
-        m/=== begin forced alignment ===/ and $in_walign = 1;
-        m/=== end forced alignment ===/ and (print {$mlf_out_fh} ".\n"), $in_walign = 0;
-        if ($in_walign and my @m = /^\[\s*(\d+)\s+(\d+)\s*\]\s*([-\d.]+)\s+(\S+)/) {
-            next if $m[3] =~ /^</;
-            print {$mlf_out_fh} $m[0].'00000 '.$m[1]."00000 $m[3] $m[2]\n";
-        }
-    }
-    close $recout_fh;
-    close $mlf_out_fh;
-    
-    my $results_fn = h(stringify_options(
-        ''   => 'HResults',
-        '-A' => '', '-D' => '', '-T' => 1,
-        '-z' => '</s>',
-        '-I' => $trans_fn,
-        ''   => [$phones_fn, $mlf_out_fn],
-    ), LANG => 'C', return_output  => 1);
-    my $line = '';
-    my $raw = '';
-    {
-        local @ARGV = $results_fn;
-        while (<ARGV>) {
-            $raw .= $_;
-            if (/Overall Results/ .. /WORD:/) {
-                $line = $_;
-            }
-        }
-    }
-    $line =~ /%Corr=(\S+?),/ or die "Unexpected results:\n$raw";
-
-    # save score next to hmmdefs
-    my $opened = open my $score_fh, '>', "$hmmdir/score";
-    if ($opened) {
-        print {$score_fh} $raw;
-        close $opened;
-    }
-    else {
-        warn "Couldn't save score to '$hmmdir/score'";
-    }
-
-    return Score->new($1, $raw);
+    return $recout_fn;
 }
 
 sub julius_parallel {
