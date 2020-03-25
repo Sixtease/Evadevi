cea43fd486648e5dfb372fa431803689e3348d77
Jan Oldřich Krůza <jan@sixtease.net>
2020-03-25 12:07:37 +0100

    add aggregate-julout to julutil command set

--- bin/julutil.pl
+++ bin/julutil.pl
@@ -26,10 +26,43 @@ sub ngrams {
     print $sep;
 }
 
+package RecoutAggr;
+
+use Encode qw(decode encode_utf8);
+my $enc = $ENV{EV_encoding};
+
+sub aggregate {
+    my $part_length_seconds = shift;
+    my $last_file = '';
+    my $offset = -$part_length_seconds;
+    while (<>) {
+        if ($ARGV ne $last_file) {
+            $offset += $part_length_seconds;
+            $last_file = $ARGV;
+        }
+        next if not /-- word alignment --/ .. /=== end forced alignment ===/;
+        my $str = decode $enc, $_;
+
+        # cs as in centiseconds
+        my ($start_cs, $end_cs, $word_uc) = $str =~ /\[\s*(\d+)\s+(\d+)\s*\]\s+\S+\s+(\S+)/ or next;
+        my $start = $offset + $start_cs / 100;
+        my $end = $offset + $end_cs / 100;
+        my $word = lc $word_uc;
+
+        next if $word =~ /</;
+
+        say encode_utf8 "$start $end $word";
+    }
+}
+
 package main;
 my $command = shift;
 if ($command eq 'sort_lm') {
     LmSort::sort_lm();
+}
+elsif ($command eq 'aggregate-julout') {
+    my $part_length_seconds = shift;
+    RecoutAggr::aggregate($part_length_seconds);
 }
 else {
     die "unknown command '$command'";
